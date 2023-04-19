e22f28175bf39cee30c2f6ca6c3ba487b18d9a88
TurunenP <k7jepu00@students.oamk.fi>
2023-04-19 09:48:14 +0300

    Data in mongodb

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
+const mongoose = require('mongoose');
+const express = require('express');
+//const router = express.Router();
+
+const app = express();
+
 const mqtt = require('mqtt')
 const fs = require('fs');
 const { reverse } = require('dns');
 const io = require('socket.io')(3000);
 // connect to MQTT broker
+
 const options = {host: 'a39cwxnxny8cvy.iot.eu-west-1.amazonaws.com',
                 port: '8883',
                 protocol: 'mqtt',
                 rejectUnauthorized: false,
-                key: fs.readFileSync('certificates/sales-cloudext-prfi00airmonitoring.key'),
-                cert: fs.readFileSync('certificates/sales-cloudext-prfi00airmonitoring.pem')
-                }
+                key: fs.readFileSync('./certificates/sales-cloudext-prfi00airmonitoring.key'),
+                cert: fs.readFileSync('./certificates/sales-cloudext-prfi00airmonitoring.pem')
+}
 
 const client = mqtt.connect(options)
+
+var Smart = 'mongodb+srv://Chiry:Chiry50@cluster0.b7cjm6q.mongodb.net/Haltian?retryWrites=true&w=majority'
+
+mongoose.connect(Smart);
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo");
+});
+
+client.on('connect', function () {
+  console.log('Connected to MQTT broker');
+});
+
+client.subscribe('cloudext/json/pr/fi/prfi00airmonitoring/#', function (error) {
+  if (error) {
+    console.error('Error subscribing to topic:', error);
+  } else {
+    console.log('Subscribed to topic');
+  }
+});
+
+const dataSchema = new mongoose.Schema({
+  data: Object
+});
+
+const User = mongoose.model('data', dataSchema);
+// Handle incoming messages
+client.on('message', function (topic, message) {
+  console.log(JSON.parse(message))
+  const user = new User( {data:JSON.parse(message)});
+  user.save().then(() => console.log('inserted data'));
+  //console.log('Received message on topic', topic, ':', message.toString());
+
+});
+
 let reading;
 
-// subscribe to a topic
-client.on('connect', () => {
-    console.log('Connected MQTT') 
-  client.subscribe('cloudext/json/pr/fi/prfi00airmonitoring/#')
-})
 
-
-
-io.on("connection", socket => {
-  // handle incoming messages
-client.on('message', (topic, message) => {
-  reading = JSON.parse(message.toString())
-  reading['timestamp'] = new Date()
-  console.log('Latest Reading', reading)
-
-  socket.emit("readings", JSON.stringify(reading));
-  console.log(`Received message on topic "${topic}"`)
-  })
-
-})
 
